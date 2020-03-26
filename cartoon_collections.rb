1a78fea9de37d1a7da6b77325b16d90188214860
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-26 00:38:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 1 
+  dwarf_array = []
+  while i <= array.length
+    array.each do |dwarf|
+      dwarf_array.push("#{i} #{dwarf}")
+      i += 1
+    end
+  end
+    puts dwarf_array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0 
+  calls = []
+  while i < array.length
+     array.collect do |word|
+       word = word.capitalize()
+       calls << ("#{word}!")
+       i += 1
+     end
+   end
+     calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
